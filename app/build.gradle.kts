a53bbbc80822a9c0cea2033cf0b9a69e7c900133
Luiz Felipe de Faria <luiz.faria@sou.unaerp.edu.br>
2025-10-31 15:50:48 -0300

    Configuração do Firebase e adição das dependências do MVVM

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -47,6 +48,18 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation("com.google.android.material:material:1.12.0")
+    implementation(platform("com.google.firebase:firebase-bom:33.1.0"))
+
+    // Autenticação
+    implementation("com.google.firebase:firebase-auth-ktx") // Banco de Dados (Firestore)
+    implementation("com.google.firebase:firebase-firestore-ktx") // Armazenamento de Imagens (Storage)
+    implementation("com.google.firebase:firebase-storage-ktx")
+
+    // --- Arquitetura MVVM (Requisito RFN) ---
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.0")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.7.3")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
